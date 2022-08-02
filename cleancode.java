cc8dd99f5906ca37623ee035c713351a399d366a
Niyolynate <niolynate@gmail.com>
2022-08-02 14:26:03 +0300

    Added beeper karal

---
+++ cleancode.java
@@ -0,0 +1,50 @@
+/*File DoubleBeeper.java
+ *......................
+ *Karal doubles the number of beepers on the coener directly in front of him in  */ the world. then returns to his original position /orientation.
+
+ import computerclass.karal.*;
+
+ public class OurDoubleBeeper extends Superkaral {
+
+       public void run() {
+	       move();
+	       DoubleTheBeeperInPile();
+	       moveBackwards();
+       }
+
+       private void moveBackwards() {               
+	       turnAround();
+	       move();
+	       turnAround();
+       }
+
+       private void DoubleBeeperInPile() {
+	       while beeperPresent() {
+		       pickBeeper();
+		       PutTwoBeepersNextDoor();
+       }
+       moveThePileNextDoorBack();
+       }
+
+       private void PutTwoBeepersNextDoor() {
+	       move();
+	       putBeeper();
+	       putBeeper();
+	       moveBackwards();
+
+	}
+       Private void moveThePileNextDoorBack() {
+	       move();
+	       while (beeperPresent()) {
+		       moveOneBeeperBack();
+	}
+	moveBackwards();
+       }
+       Private void moveOneBeeperBack() {
+	       pickBeeper();
+               moveBackwards();
+	       putBeeper();
+	       move();
+
+     }
+ }
